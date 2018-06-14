e2ad84cab0e839d6c867aac001866e7913b3073a
Alia Wilkinson <aliawilkinson@gmail.com>
2018-06-13 17:24:08 -0700

    added chat functionality

--- src/components/app.js
+++ src/components/app.js
@@ -5,6 +5,7 @@ import Home from './home';
 import { Route } from 'react-router-dom';
 import Chat from './chat';
 import CreateChatRoom from './create_chat_room';
+import ChatRooms from './chat_rooms';
 
 const App = () => (
     <div>
@@ -13,6 +14,7 @@ const App = () => (
             <Route exact path="/" component={Home} />
             <Route path="/chat/:id" component={Chat} />
             <Route path="/create-room" component={CreateChatRoom} />
+            <Route path="/chat-rooms" component={ChatRooms} />
         </div>
     </div>
 );
